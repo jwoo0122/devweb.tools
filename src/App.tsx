cd7fdb2fc66464802c2892a503b89ff8b0cba153
Jinwoo Jeong <thinkratomos@gmail.com>
2024-03-26 02:46:16 +0900

    basic styling

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,6 @@
-import "./App.css";
-import { Title } from "./components/Title";
-import toolsjson from "./assets/tools.json";
-import { ToolDot } from "./components/ToolDot";
+import { Title } from "src/components/Title";
+import toolsjson from "src/assets/tools.json";
+import { ToolDot } from "src/components/ToolDot";
 import gsap from "gsap";
 import { useGSAP } from "@gsap/react";
 
@@ -14,18 +13,25 @@ function App() {
     gsap.from(".toolDot", {
       stagger: 0.1,
       opacity: 0,
-      x: -100,
-      duration: 0.5,
-      ease: "expo.inOut",
+      y: 10,
+      duration: 0.3,
+      ease: "back.inOut",
     });
   });
 
   return (
     <>
-      <Title />
-      {tools.map(([name, datas]) => (
-        <ToolDot key={name} name={name} {...datas} />
-      ))}
+      <div
+        style={{
+          padding: "2rem",
+        }}
+      >
+        {/* TODO: I strongly want to use full screen, this is a temporal styles */}
+        <Title />
+        {tools.map(([name, datas]) => (
+          <ToolDot key={name} name={name} {...datas} />
+        ))}
+      </div>
     </>
   );
 }
---
+++ src/components/ToolDot/index.ts
@@ -0,0 +1,1 @@
+export { ToolDot } from "./ToolDot";
